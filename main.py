a3584024b28984967e67ddae1dc94f7b7fa44ba2
Məhəmməd Bayramov <mamed.bayramov.02@gmail.com>
2023-02-25 17:12:48 +0400

    caculator elave edildi

---
+++ main.py
@@ -0,0 +1,13 @@
+num1 = int(input('Enter a number: '))
+num2 = int(input ('Enter another number: '))
+opr = input("Enter an operation ")
+if opr == "+":
+    print(num1 + num2 )
+elif opr == "/":
+    print(num1 / num2)
+elif opr == "-":
+    print(num1 - num2)
+elif opr == "*":
+    print(num1 * num2)
+else:
+    print("invalid operation ")
